3dd2475871e05c9b49d3ba3a430f8553b41eb47c
Marina <artyusmarina@yandex.ru>
2021-03-11 21:09:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+	if (n < 0) {
+		// let minus = n.toString().split('').shift();
+		let a = n.toString().split('').reverse();
+		a.pop();
+		// a.unshift(minus);
+		
+		return +a.join('');
+	} else {
+		return +n.toString().split('').reverse().join('');
+	}
+
 }
